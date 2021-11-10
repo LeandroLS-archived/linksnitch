ab4161465fb8e75ce00e6b956371251e5c6ede25
Leandro Lima <llimas@outlook.com>
2021-11-10 19:35:10 -0300

    logic to get bad links

--- action-a/main.go
+++ action-a/main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/gomarkdown/markdown"
+	"golang.org/x/net/html"
 )
 
 func main() {
-	fmt.Println("Olá")
-	dat, err := os.ReadFile("README.md")
+	markDown, err := os.ReadFile("README.md")
 	check(err)
-	fmt.Print(string(dat))
+	htmlBytes := markdown.ToHTML(markDown, nil, nil)
+	bReader := bytes.NewReader(htmlBytes)
+	htmlParsed, err := html.Parse(bReader)
+	check(err)
+	links := GetHtmlTags(htmlParsed, "a", "href", nil)
+	var badLinks []string
+	if len(links) < 1 {
+		fmt.Println("No links found.")
+	} else {
+		for i := 0; i < len(links); i++ {
+			fmt.Println(links)
+			resp, err := http.Get(links[i])
+			if err != nil {
+				badLinks = append(badLinks, links[i])
+				continue
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				badLinks = append(badLinks, links[i])
+			}
+		}
+	}
+	if len(badLinks) >= 1 {
+		log.Fatalf("Bad Links %s", badLinks)
+	}
+}
 
+func GetHtmlTags(n *html.Node, rawHtmlTag string, htmlTagKey string, tags []string) []string {
+	if n.Type == html.ElementNode && n.Data == rawHtmlTag {
+		for _, a := range n.Attr {
+			if a.Key == htmlTagKey {
+				tags = append(tags, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		tags = GetHtmlTags(c, rawHtmlTag, htmlTagKey, tags)
+	}
+	return tags
 }
 
 func check(e error) {
